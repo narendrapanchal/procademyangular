85bdca729b29b3504f891f7b22896aacc8af7f48
Narendra Kumar Panchal <narendra@happycredit.in>
2024-01-12 13:09:22 +0530

    input to output= output decorator and input decorator

---
+++ src/app/header/container/product-list/filter/filter.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent {
+  @Input()
+all:number=0;
+  @Input()
+  inStock:number=0;
+
+  @Input()
+  outOfStock:number=0;
+  selectedFilterRadioButton:string= "all";
+  @Output()
+  selectedFilterRadioButtonChanged:EventEmitter<string>=new EventEmitter<string>();
+  onSelectedFilterRadioButtonChanged(){
+    console.log("this.selectedFilterRadioButton",this.selectedFilterRadioButton)
+    this.selectedFilterRadioButtonChanged.emit(this.selectedFilterRadioButton);
+  }
+}
